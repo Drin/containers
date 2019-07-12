d4af2ab5c2d4019cd7ff3e57ec8569cc5be6dfd4
Aldrin <DrinMontana@acm.org>
2019-07-11 17:19:02 -0700

    now preferring arch-pybase to pybase due to difficulties with musl in alpine. Also split bashrc into bash_environment and bash_init and downloaded get-poetry file so that it doesn't have to be downloaded. entrypoint scripts are deprecated. may update dockerfiles with CMD default, but for now they just define setup

--- dockerfiles/arch-pybase/scripts/install-pyenv.bash
+++ dockerfiles/arch-pybase/scripts/install-pyenv.bash
@@ -2,8 +2,6 @@
 set -x
 
 # set pyenv path and checkout repo to that path
-mkdir -p "/toolbox"
-
 pyenv_path="/toolbox/pyenv"
 git clone https://github.com/pyenv/pyenv $pyenv_path
 
@@ -13,3 +11,7 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 
 # init pyenv if it isn't already
 eval "$(pyenv init -)"
+
+# install and set python version
+pyenv install 3.7.3
+pyenv global  3.7.3
--- dockerfiles/pybase/scripts/entrypoint.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-set -e
-set -x
-
-source $HOME/.bash_environment
